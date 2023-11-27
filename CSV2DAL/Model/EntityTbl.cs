dc4fcdbc3726205b9c251da3f503cfced09c7afc
NehaAralimatti <nehaaralimatti@gmail.com>
2023-11-27 22:05:18 +0530

    CSV2DAL Added

---
+++ CSV2DAL/Model/EntityTbl.cs
@@ -0,0 +1,20 @@
+﻿using CSVFileDAL;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSVFileDAL.Models;
+
+public class EntityTbl
+{
+    [Key]
+    public string EntityName { get; set; }
+    public string Description{ get; set; }
+
+    public string FeatureName { get; set; }
+
+    public string FeatureDataType { get; set; }
+    public string FeatureValue {  get; set; }
+
+    
+}
+
+
