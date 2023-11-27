e3f45fd55f9435fadc09e0550df17b2b9b00f5d8
NehaAralimatti <nehaaralimatti@gmail.com>
2023-11-27 22:20:46 +0530

    React apps

---
+++ React Apps/featurestat/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CsvUploader from './CsvUploader';
+
+
+
+const App = () => {
+  return (
+    <div>
+      
+     
+      <CsvUploader/>
+      {/* <CSVFileUploader/> */}
+    </div>
+  );
+};
+
+export default App;
